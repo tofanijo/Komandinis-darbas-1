c47a45b464a2c14680c78e5dadfa107759c35fed
tofanijo <tofanijo@gmail.com>
2020-01-04 14:29:31 +0200

    klaidu taisymas 3

--- Zaidimas1/Zaidimas1/Program.cs
+++ Zaidimas1/Zaidimas1/Program.cs
@@ -13,14 +13,16 @@ namespace Zaidimas1
             double zemesRutulioPlotas = 0;
             double zemesRutulioTuris = 0;
             double zemesRutulioSpindulys = 6371;
-            
+            double trikampioIzambine = 0;
+            double statinis1 = 10;
+            double statinis2 = 15;
 
-            
-           
+
 
 
             zemesRutulioPlotas = 4 * Math.PI * Math.Pow(zemesRutulioSpindulys, 2);
             zemesRutulioTuris = (double)((double)4 / (double)3) * Math.PI * Math.Pow(zemesRutulioSpindulys, 3);
+            trikampioIzambine = Math.Sqrt(Math.Pow(statinis1, 2) + Math.Pow(statinis2, 2));
 
             Console.WriteLine("*********************************************");
             Console.WriteLine($"Zemes rutulio turis yra: {zemesRutulioPlotas}");
