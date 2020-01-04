b00d7355fb575a5f88cecf25ed8aad85a87b32a0
andriuskin111 <andriuskin@gmail.com>
2020-01-04 14:27:25 +0200

    Prideta trecia uzduotis

--- Zaidimas1/Zaidimas1/Program.cs
+++ Zaidimas1/Zaidimas1/Program.cs
@@ -13,7 +13,7 @@ namespace Zaidimas1
             double zemesRutulioPlotas = 0;
             double zemesRutulioTuris = 0;
             double zemesRutulioSpindulys = 6371;
-            
+            double trikampioIzambine = 0;
 
             
            
@@ -22,9 +22,12 @@ namespace Zaidimas1
             zemesRutulioPlotas = 4 * Math.PI * Math.Pow(zemesRutulioSpindulys, 2);
             zemesRutulioTuris = (double)((double)4 / (double)3) * Math.PI * Math.Pow(zemesRutulioSpindulys, 3);
 
+
+
             Console.WriteLine("*********************************************");
             Console.WriteLine($"Zemes rutulio turis yra: {zemesRutulioPlotas}");
             Console.WriteLine($"Zemes rutulio turis yra: {zemesRutulioTuris}");
+            Console.WriteLine($"Trikampio izambine yra: {trikampioIzambine}");
             Console.WriteLine("*********************************************");
 
             Console.ReadLine();
